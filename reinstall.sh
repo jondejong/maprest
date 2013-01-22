0c2b761e914362eb8f7c84393241eb34e9493c55
Jon DeJong <jon.dejong@gmail.com>
2013-01-21 19:21:57 -0600

    removing environment specific build script

--- reinstall.sh
+++
@@ -1,8 +0,0 @@
-grails clean
-grails package-plugin
-cp -v grails-maprest-$1.zip ~/Projects/easyrest/
-cd ~/Projects/easyrest/
-#grails uninstall-plugin maprest
-grails clean
-grails install-plugin grails-maprest-$1.zip
-cd ~/Projects/maprest/
