a98579f3d7bef5711f54730c4e89b23680412112
Burt Beckwith <burt@burtbeckwith.com>
2013-02-09 18:26:22 -0500

    cleanup

--- MaprestGrailsPlugin.groovy
+++ MaprestGrailsPlugin.groovy
@@ -1,53 +1,27 @@
-import com.jondejong.maprest.MaprestFormat
 import grails.converters.JSON
+
 import org.codehaus.groovy.grails.plugins.web.api.ControllersApi
 import org.codehaus.groovy.grails.web.metaclass.RenderDynamicMethod
 
+import com.jondejong.maprest.MaprestFormat
+
 class MaprestGrailsPlugin {
 
-    // the plugin version
     def version = "0.1.0"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
 
     def loadAfter = ['controllers']
     def observe = ["controllers"]
 
-    // TODO Fill in these fields
-    def title = "Maprest Plugin" // Headline display name of the plugin
+    def title = "Maprest Plugin"
     def author = "Jon DeJong"
     def authorEmail = ""
-    def description = '''\
-Allows customization of REST responses using property maps.
-'''
-
-    // URL to the plugin's documentation
+    def description = 'Allows customization of REST responses using property maps.'
     def documentation = "https://github.com/jondejong/maprest"
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
+    def issueManagement = [ system: "Github", url: "https://github.com/jondejong/maprest/issues" ]
     def scm = [url: "https://github.com/jondejong/maprest"]
-
-//    def watchedResources = 'file:./grails-app/controllers'
 
     def doWithDynamicMethods = { ctx ->
         addMethodsToControllers(application)
@@ -193,26 +167,5 @@ Allows customization of REST responses using property maps.
                 builder."${entry.key}"(entry.value)
             }
         }
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
     }
 }
--- src/groovy/com/jondejong/maprest/MaprestFormat.groovy
+++ src/groovy/com/jondejong/maprest/MaprestFormat.groovy
@@ -1,11 +1,7 @@
 package com.jondejong.maprest
 
 /**
- * Created with IntelliJ IDEA.
- * User: jondejong
- * Date: 1/21/13
- * Time: 6:49 PM
- * To change this template use File | Settings | File Templates.
+ * @author jondejong
  */
 class MaprestFormat {
 
