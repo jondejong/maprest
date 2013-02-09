41390b44173457192d3fe329dfb4bebfcf5b1209
Burt Beckwith <burt@burtbeckwith.com>
2013-02-09 18:45:37 -0500

    moved rendering logic into its own class

--- MaprestGrailsPlugin.groovy
+++ MaprestGrailsPlugin.groovy
@@ -1,9 +1,5 @@
-import grails.converters.JSON
-
-import org.codehaus.groovy.grails.plugins.web.api.ControllersApi
-import org.codehaus.groovy.grails.web.metaclass.RenderDynamicMethod
-
 import com.jondejong.maprest.MaprestFormat
+import com.jondejong.maprest.MaprestRenderer
 
 class MaprestGrailsPlugin {
 
@@ -32,139 +28,12 @@ class MaprestGrailsPlugin {
     }
 
     def addMethodsToControllers(application) {
+        MaprestRenderer renderer = new MaprestRenderer()
         for (controllerClass in application.controllerClasses) {
             controllerClass.metaClass.getXmlFormat = { MaprestFormat.Format.XML }
             controllerClass.metaClass.getJsonFormat = { MaprestFormat.Format.JSON }
             controllerClass.metaClass.renderMaprest = { Object o, MaprestFormat.Format f, String root = null ->
-
-                def map = o.transformToMap()
-
-                if (f == MaprestFormat.Format.XML) {
-                    if (!root) {
-                        root = 'root'
-                    }
-                    renderXml(map, root)
-                } else if (f == MaprestFormat.Format.JSON) {
-                    if (root) {
-                        map = ["${root}": map]
-                    }
-                    renderJson map
-                }
-            }
-        }
-    }
-
-    def renderXml(map, root) {
-        def renderDynamicMethod = new RenderDynamicMethod()
-        def rootAttributes = parseRootAttributes(map)
-        renderDynamicMethod.invoke(this, "render", [contentType: "application/xml"], {
-            "${root}" (rootAttributes){
-                mapAsXml(delegate, map)
-            }
-        })
-    }
-
-    def parseRootAttributes(Map map) {
-        def attributes = [:]
-        def attributeKeys = []
-
-        map.keySet().each { String key ->
-            if (key.startsWith('@')) {
-                String attribute = key[1..key.size() - 1]
-                attributes.put(attribute, map.get(key))
-                attributeKeys.add(key)
-            }
-        }
-
-        attributeKeys.each {map.remove(it)}
-
-        return attributes
-    }
-
-    def renderJson(map) {
-        def scrubbedMap = scrubObjectForJson(map)
-        def renderDynamicMethod = new RenderDynamicMethod()
-
-        def api = new ControllersApi()
-        api.render(this, scrubbedMap as JSON)
-    }
-
-
-    def scrubObjectForJson(map) {
-        def newMap = [:]
-        if (map instanceof Collection) {
-            def newCollection = []
-            map.each {
-                newCollection.add scrubObjectForJson(it)
-            }
-            return newCollection
-        } else {
-            map.keySet().each {
-                def val = map.get it
-                def key = it
-                if (it.toString().startsWith('@')) {
-                    key = key[1..key.size() - 1]
-                }
-                if (val instanceof Map || val instanceof Collection) {
-                    val = scrubObjectForJson(val)
-                }
-                newMap.put(key, val)
-            }
-        }
-        return newMap
-    }
-
-    protected mapAsXml(builder, map) {
-        for (entry in map) {
-            if (entry.value instanceof Map) {
-                def attributes = [:]
-                def attributeKeys = []
-
-                def element = (Map) entry.value
-
-                element.keySet().each { String key ->
-                    if (key.startsWith('@')) {
-                        String attribute = key[1..key.size() - 1]
-                        attributes.put(attribute, element.get(key))
-                        attributeKeys.add(key)
-                    }
-                }
-                attributeKeys.each {
-                    element.remove(it)
-                }
-                builder."${entry.key}"(attributes) {
-                    mapAsXml(builder, entry.value)
-                }
-
-            } else if (entry.value instanceof Collection) {
-                builder."${entry.key}" {
-                    for (childMap in entry.value) {
-                        def attributes = [:]
-                        def attributeKeys = []
-                        def elementName
-                        if (childMap?.containsKey('elementName')) {
-                            elementName = childMap.elementName
-                            childMap.remove 'elementName'
-                        } else {
-                            elementName = "${entry.key[0..-2]}"
-                        }
-                        childMap.keySet().each { key ->
-                            if (key.startsWith('@')) {
-                                String attribute = key[1..key.size() - 1]
-                                attributes.put(attribute, childMap.get(key))
-                                attributeKeys.add(key)
-                            }
-                        }
-
-                        attributeKeys.each { childMap.remove(it) }
-
-                        "${elementName}"(attributes) {
-                            mapAsXml(builder, childMap)
-                        }
-                    }
-                }
-            } else {
-                builder."${entry.key}"(entry.value)
+                renderer.renderMaprest o, f, root
             }
         }
     }
---
+++ src/groovy/com/jondejong/maprest/MaprestRenderer.groovy
@@ -0,0 +1,136 @@
+package com.jondejong.maprest
+
+import grails.converters.JSON
+
+import org.codehaus.groovy.grails.plugins.web.api.ControllersApi
+import org.codehaus.groovy.grails.web.metaclass.RenderDynamicMethod
+
+class MaprestRenderer {
+
+    protected RenderDynamicMethod renderDynamicMethod = new RenderDynamicMethod()
+    protected ControllersApi api = new ControllersApi()
+
+    def renderMaprest(Object o, MaprestFormat.Format f, String root = null) {
+
+        Map map = o.transformToMap()
+
+        if (f == MaprestFormat.Format.XML) {
+            if (!root) {
+                root = 'root'
+            }
+            renderXml(map, root)
+        }
+        else if (f == MaprestFormat.Format.JSON) {
+            if (root) {
+                map = ["${root}": map]
+            }
+            renderJson map
+        }
+    }
+
+    def renderXml(Map map, root) {
+        def rootAttributes = parseRootAttributes(map)
+        renderDynamicMethod.invoke this, "render", [contentType: "application/xml"], {
+            "$root" (rootAttributes) {
+                mapAsXml(delegate, map)
+            }
+        }
+    }
+
+    protected Map parseRootAttributes(Map map) {
+        def attributes = [:]
+        def attributeKeys = []
+
+        map.each { String key, value ->
+            if (key.startsWith('@')) {
+                String attribute = key[1..key.size() - 1]
+                attributes[attribute] = value
+                attributeKeys << key
+            }
+        }
+
+        attributeKeys.each { map.remove(it) }
+
+        return attributes
+    }
+
+    def renderJson(map) {
+        def scrubbedMap = scrubObjectForJson(map)
+        api.render(this, scrubbedMap as JSON)
+    }
+
+    def scrubObjectForJson(map) {
+        if (map instanceof Collection) {
+            return map.collect { scrubObjectForJson(it) }
+        }
+
+        def newMap = [:]
+        map.each { key, val ->
+            if (key.toString().startsWith('@')) {
+                key = key[1..key.size() - 1]
+            }
+            if (val instanceof Map || val instanceof Collection) {
+                val = scrubObjectForJson(val)
+            }
+            newMap[key] = val
+        }
+        return newMap
+    }
+
+    protected mapAsXml(builder, Map map) {
+        for (Map.Entry entry in map) {
+            if (entry.value instanceof Map) {
+                def attributes = [:]
+                def attributeKeys = []
+
+                def element = entry.value
+
+                element.keySet().each { String key ->
+                    if (key.startsWith('@')) {
+                        String attribute = key[1..key.size() - 1]
+                        attributes[attribute] = element.get(key)
+                        attributeKeys.add(key)
+                    }
+                }
+                attributeKeys.each {
+                    element.remove(it)
+                }
+                builder."${entry.key}"(attributes) {
+                    mapAsXml(builder, entry.value)
+                }
+            }
+            else if (entry.value instanceof Collection) {
+                builder."${entry.key}" {
+                    for (childMap in entry.value) {
+                        def attributes = [:]
+                        def attributeKeys = []
+                        def elementName
+                        if (childMap?.containsKey('elementName')) {
+                            elementName = childMap.elementName
+                            childMap.remove 'elementName'
+                        }
+                        else {
+                            elementName = "${entry.key[0..-2]}"
+                        }
+                        childMap.keySet().each { key ->
+                            if (key.startsWith('@')) {
+                                String attribute = key[1..key.size() - 1]
+                                attributes[attribute] = childMap.get(key)
+                                attributeKeys.add(key)
+                            }
+                        }
+
+                        attributeKeys.each { childMap.remove(it) }
+
+                        "${elementName}"(attributes) {
+                            mapAsXml(builder, childMap)
+                        }
+                    }
+                }
+            }
+            else {
+                builder."${entry.key}"(entry.value)
+            }
+        }
+    }
+}
